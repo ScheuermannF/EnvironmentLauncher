38d1b7082131c353e8d5bfac04d2bd813eed41d5
Francis Scheuermann <francis.scheuermann@schoolmint.net>
2022-05-10 15:30:46 -0500

    Initial Commit

---
+++ EnvironmentLauncher/EnvironmentLauncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentLauncherApp.swift
+//  EnvironmentLauncher
+//
+//  Created by Francis Scheuermann on 5/10/22.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentLauncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EnvironmentLauncherUITests/EnvironmentLauncherUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EnvironmentLauncherUITestsLaunchTests.swift
+//  EnvironmentLauncherUITests
+//
+//  Created by Francis Scheuermann on 5/10/22.
+//
+
+import XCTest
+
+class EnvironmentLauncherUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
